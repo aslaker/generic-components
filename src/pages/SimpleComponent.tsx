3bd88791a056b8b186d440f2bf0849e14062e4f0
aslaker <51129804+aslaker@users.noreply.github.com>
2021-11-11 02:20:08 +0000

    Add classnames

--- src/pages/SimpleComponent.tsx
+++ src/pages/SimpleComponent.tsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 
 interface SimpleComponentProps {
   toggleItems: string[];
+  onToggleChange?: (activeToggleName: string) => any;
 }
 
-const SimpleComponent: React.FC<SimpleComponentProps> = ({ toggleItems }) => {
+const SimpleComponent: React.FC<SimpleComponentProps> = ({
+  toggleItems = [],
+  onToggleChange = () => {},
+}) => {
+  const [toggles, setToggles] = useState(() => {
+    return toggleItems.map((item) => ({
+      name: item,
+      active: false,
+    }));
+  });
+
+  const onToggleClick = (toggleName: string) => {
+    const newTogglesState = toggles.map((toggle) => {
+      if (toggle.name === toggleName) {
+        toggle.active = true;
+        onToggleChange(toggle.name);
+      } else {
+        toggle.active = false;
+      }
+      return toggle;
+    });
+    setToggles(newTogglesState);
+  };
+
   return (
     <div className="flex-center bg-gray-200">
       <div className="flex justify-around items-center bg-white rounded-full w-96">
-        {toggleItems.map((item) => (
-          <div className="flex-center flex-1 p-5">
-            <span>{item}</span>
+        {toggles.map((item) => (
+          <div
+            className={classNames("flex-center", "flex-1", "p-5", {
+              "rounded-full": item.active,
+              "bg-blue-800": item.active,
+              "text-white": item.active,
+            })}
+            onClick={() => onToggleClick(item.name)}
+          >
+            <span>{item.name}</span>
           </div>
         ))}
       </div>
